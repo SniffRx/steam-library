5ca4c47520731cd675be618c19d37480164b3333
SniffRx <fortik.sindroms@yandex.ru>
2025-05-30 17:49:37 +0300

    Added motion effects on app.tsx

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,6 +5,9 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
 
+import { motion, AnimatePresence } from 'framer-motion';
+import { LoadingSpinner } from '@/components/ui/loading-spinner';
+
 // Типизация страницы
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -14,12 +17,32 @@ createInertiaApp({
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(
+            <AnimatePresence mode="wait">
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 0.3 }}
+                    // key={props.page.component}
+                >
+                    <App {...props} />
+                </motion.div>
+            </AnimatePresence>
+        );
     },
     progress: {
-        color: '#4B5563'
+        color: '#66C0F4',
+        showSpinner: true,
+        delay: 250,
+        includeCSS: true,
+        spinner: (
+            <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <LoadingSpinner className="w-12 h-12 text-[#66C0F4]" />
+            </div>
+        )
     }
-});
+}).then(r => {return(r)});
 
 // This will set light / dark mode on load...
 initializeTheme();
