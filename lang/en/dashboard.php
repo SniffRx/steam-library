39ca459713b377e7b1e59bea7175fa3fb3b25468
SniffRx <fortik.sindroms@yandex.ru>
2025-10-25 23:46:21 +0300

    Added lang

---
+++ lang/en/dashboard.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'connect_steam_required' => 'Please connect your Steam account first',
+    'connect_steam_button' => 'Connect Steam',
+    'invalid_steam_id' => 'Your Steam ID is invalid. Please reconnect your account.',
+    'reconnect_steam_button' => 'Reconnect Steam',
+    'rate_limit_exceeded' => 'Too many requests. Please try again later.',
+    'error_loading' => 'Failed to load dashboard',
+    'try_again' => 'Try Again',
+];
---
+++ lang/ru/dashboard.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'connect_steam_required' => 'Пожалуйста, подключите ваш Steam аккаунт',
+    'connect_steam_button' => 'Подключить Steam',
+    'invalid_steam_id' => 'Ваш Steam ID недействителен. Переподключите аккаунт.',
+    'reconnect_steam_button' => 'Переподключить Steam',
+    'rate_limit_exceeded' => 'Слишком много запросов. Попробуйте позже.',
+    'error_loading' => 'Не удалось загрузить панель управления',
+    'try_again' => 'Попробовать снова',
+];
